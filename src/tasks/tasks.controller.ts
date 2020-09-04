d5ab2922cabf8d0f7e1416e8dea5f8d812655e6f
sangnn <sang.nguyen@eye-solution.vn>
2020-09-04 10:42:01 +0700

    Get all task & query

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,14 +9,10 @@ import { TaskStatus } from './task.status.enum';
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 
-    // @Get()
-    // getAllTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto):Task[] {
-    //     if(Object.keys(filterDto).length) {
-    //         return this.tasksService.getTasksWithFilters(filterDto);
-    //     } else {
-    //         return this.tasksService.getAllTasks();
-    //     }
-    // }
+    @Get()
+    getAllTasks(@Query() filterDto: GetTasksFilterDto): Promise<Task[]> {
+        return this.tasksService.getTasks(filterDto);
+    }
 
     @Get('/:id')
     getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,29 +12,9 @@ export class TasksService {
         private taskRepository: TaskRepository
     ) {}
 
-
-
-    // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    //     const { status, search } = filterDto;
-    //     let filteredTasks = this.getAllTasks();
-
-    //     if (status) {
-    //         filteredTasks = filteredTasks.filter(t => t.status === status);
-    //     }
-
-    //     if (search) {
-    //         filteredTasks = filteredTasks.filter(t =>
-    //             t.title.includes(search) ||
-    //             t.description.includes(search)
-    //         )
-    //     }
-
-    //     return filteredTasks;
-    // }
-
-    // getAllTasks() {
-    //     return this.tasks;
-    // }
+    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+        return this.taskRepository.getTasks(filterDto);
+    }
 
     async getTaskById(id: number): Promise<Task> {
         const found = await this.taskRepository.findOne(id);
